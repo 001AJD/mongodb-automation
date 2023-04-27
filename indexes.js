3853253adbdc44958b6f95bfa356a3acbc6eb04c
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-04-27 10:27:45 +0530

    removed 1 index

--- indexes.js
+++ indexes.js
@@ -1,7 +1,6 @@
 db.getCollection("CollectionName").createIndex( { "field1" : 1 }, { background : true } );
 db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
 db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
-db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
 
 
 db.getCollection("Users").createIndex( { "firstName" : 1 }, { background : true } );
