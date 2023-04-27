3dadb519414666c159439bf2ef35d4f7bfb02798
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-04-27 10:14:24 +0530

    added indexes

--- indexes.js
+++ indexes.js
@@ -2,3 +2,8 @@ db.getCollection("CollectionName").createIndex( { "field1" : 1 }, { background :
 db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
 db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
 db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
+
+
+db.getCollection("Users").createIndex( { "firstName" : 1 }, { background : true } );
+db.getCollection("Products").createIndex( { "productName" : 1 }, { background : true } );
+db.getCollection("Plans").createIndex( { "planId" : 1 }, { background : true } );
