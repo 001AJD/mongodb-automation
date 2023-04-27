65fa47d982b013176b258496227b812d90331421
Ajinkya Dhomne <ajinkyadhomne.27@gmail.com>
2023-04-27 10:08:20 +0530

    added new index

--- indexes.js
+++ indexes.js
@@ -1,3 +1,4 @@
 db.getCollection("CollectionName").createIndex( { "field1" : 1 }, { background : true } );
 db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
 db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
+db.getCollection("CollectionName2").createIndex( { "field2" : 1 }, { background : true } );
